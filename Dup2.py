c77df4fcac2152be46d2de80a901ce6e3bfda07f
BradpuppyC <rayray21211@gmail.com>
2024-01-14 15:04:12 +0000

    2

---
+++ Dup2.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        unique_set = set()
+
+        for num in nums:
+            if num in unique_set:
+                return True
+            else:
+                unique_set.add(num)
+        # If loop complete without True
+        return False
+        
+
+def getInput():
+    input_string = input("Please enter int list split by space: ")
+    input_array = list(map(int, input_string.split()))
+    return input_array
+
+def main():
+    user_array = getInput()
+    solution_instance = solution()
+    result = solution_instance.containsDuplicate(user_array)
+
+    print(f"Does array contains dup? {'Yes' if result else 'No'}")
+main()
