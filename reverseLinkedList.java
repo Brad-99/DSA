556f2c87dba4153cecee80de01f22bba1a5390b4
BradpuppyC <rayray21211@gmail.com>
2024-01-28 22:00:29 +0000

    rll

---
+++ reverseLinkedList.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class reverseLinkedList {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input: Creating a linked list
+        System.out.println("Enter the number of nodes in the linked list:");
+        int n = scanner.nextInt();
+
+        System.out.println("Enter the values of the linked list nodes:");
+        ListNode head = null;
+        ListNode tail = null;
+
+        for (int i = 0; i < n; i++) {
+            int value = scanner.nextInt();
+            ListNode newNode = new ListNode(value);
+
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+
+        // Reversing the linked list
+        ListNode reversedList = reverseList(head);
+
+        // Output: Displaying the reversed linked list
+        System.out.println("Reversed Linked List:");
+        while (reversedList != null) {
+            System.out.print(reversedList.val + " ");
+            reversedList = reversedList.next;
+        }
+
+        scanner.close();
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+}
